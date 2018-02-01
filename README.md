9733574cb608f028d9995cbcbd4e2845ff9cbadb
Marc-Eli Faldas <marc.eli.faldas@gmail.com>
2018-02-01 17:20:54 -0500

    Made Corrections

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Practice repository for DWA
 <https://dwa15.com>
-
+Today is Feb 1rst.
--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php
-	<h1>Hellow World!</h1>
-	<img src = '/images/kitten.jpg'>
-?>
+<h1>Hellow World!</h1>
+
+<img src = '/images/kitten.jpg'>
